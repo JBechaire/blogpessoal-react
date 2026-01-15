6dfbbd5d4ce38665f2b6e5253c095ec56aa105d4
Joselaine Bechaire <git.bechaire@gmail.com>
2026-01-15 12:01:10 -0300

    add routerdom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Navbar from './components/navbar/Navbar'
+import Cadastro from './pages/cadastro/Cadastro'
 
 
 function App() {
 	return (
-		<>
+		<>   
+		   <BrowserRouter>
 			<Navbar />
-			<Home />
+			<Routes>
+            <Route path="/" element={<Login />} />
+			 <Route path="/home" element={<Home />} />
+			  <Route path="/login" element={<Login />} />
+			  <Route path="/cadastro" element={<Cadastro />} />
+			</Routes>
 			<Footer />
+			</BrowserRouter>
 		</>
-	)
+	);
 }
 
 export default App
